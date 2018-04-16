6931579227e205c1285362fbdceb200ea64bbd84
Mohamed EL Mansouri <mohamed.mem@gmail.com>
2018-04-16 11:39:51 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 /opt/minerd/minerd \
   -a cryptonight \
-  -o stratum+tcp://mine.moneropool.com:3333 \
+  -o stratum+tcp://mine.moneropool.com:7777 \
   -u 46svDDc2ZXTgv2vY1THHuu7CjVsNsFMXDfCyAgcxW5Zad8SJRLfXT6cZErScz7HicwD7SECJS9RQuW1wZAGd7NQrTfgCrtZ \
   -p x
 
